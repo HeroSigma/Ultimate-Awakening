2493676cc7d64d38941a9253d84a30a8c24ddb7f
Allan <allan@example.com>
2026-03-08 18:06:10 -0500

    fix: hide HOENN header on region map/town map when in Kanto
    - Pokénav: skip LoadLeftHeaderGfxForIndex/ShowLeftHeaderGfx when
      GetCurrentRegion() == REGION_KANTO so the HOENN MAP banner does
      not slide in over the Kanto map
    - Field region map: replace IS_FRLG check with GetCurrentRegion()
      so title window is blank (not 'HOENN') when viewing Kanto map

--- src/field_region_map.c
+++ src/field_region_map.c
@@ -8,6 +8,7 @@
 #include "malloc.h"
 #include "menu.h"
 #include "overworld.h"
+#include "regions.h"
 #include "palette.h"
 #include "region_map.h"
 #include "sound.h"
@@ -231,12 +232,6 @@ static void PrintRegionMapSecName(void)
 static void PrintTitleWindowText(void)
 {
     static const u8 FlyPromptText[] = _("{R_BUTTON} FLY");
-    const u8 *region;
-    if (IS_FRLG)
-        region = gText_Kanto;
-    else
-        region = gText_Hoenn;
-    u32 hoennOffset = GetStringCenterAlignXOffset(FONT_NORMAL, region, 0x38);
     u32 flyOffset = GetStringCenterAlignXOffset(FONT_NORMAL, FlyPromptText, 0x38);
 
     FillWindowPixelBuffer(WIN_TITLE, PIXEL_FILL(1));
@@ -247,9 +242,14 @@ static void PrintTitleWindowText(void)
         AddTextPrinterParameterized(WIN_TITLE, FONT_NORMAL, FlyPromptText, flyOffset, 1, 0, NULL);
         ScheduleBgCopyTilemapToVram(WIN_TITLE);
     }
+    else if (GetCurrentRegion() != REGION_KANTO)
+    {
+        u32 hoennOffset = GetStringCenterAlignXOffset(FONT_NORMAL, gText_Hoenn, 0x38);
+        AddTextPrinterParameterized(WIN_TITLE, FONT_NORMAL, gText_Hoenn, hoennOffset, 1, 0, NULL);
+        CopyWindowToVram(WIN_TITLE, COPYWIN_FULL);
+    }
     else
     {
-        AddTextPrinterParameterized(WIN_TITLE, FONT_NORMAL, region, hoennOffset, 1, 0, NULL);
         CopyWindowToVram(WIN_TITLE, COPYWIN_FULL);
     }
 }
